dcef59a615b42f912154cb2cbd1434b9d19661e3
mnadev <mohammednadeem902@gmail.com>
2023-05-10 22:36:36 -0400

    added test data for masjids

--- test_infra/test_data.go
+++ test_infra/test_data.go
@@ -1,10 +1,12 @@
 package test_infra
 
 import (
-	userservicepb "github.com/mnadev/limestone/user_service/proto"
+	mpb "github.com/mnadev/limestone/masjid_service/proto"
+	upb "github.com/mnadev/limestone/user_service/proto"
 )
 
 const (
+	DefaultId   = "00000000-0000-0000-0000-000000000000"
 	UserEmail   = "example@example.com"
 	Password    = "password"
 	BadPassword = "passwor"
@@ -12,16 +14,27 @@ const (
 	FirstName   = "John"
 	LastName    = "Doe"
 	PhoneNumber = "+1234567890"
+	MasjidName  = "Masjid 1"
 )
 
-func GetUserProto(email string, username string) *userservicepb.User {
-	return &userservicepb.User{
-		UserId:      "00000000-0000-0000-0000-000000000000",
+func GetUserProto(email string, username string) *upb.User {
+	return &upb.User{
+		UserId:      DefaultId,
 		Email:       email,
 		Username:    username,
 		FirstName:   FirstName,
 		LastName:    LastName,
 		PhoneNumber: PhoneNumber,
-		Gender:      userservicepb.User_FEMALE,
+		Gender:      upb.User_FEMALE,
+	}
+}
+
+func GetMasjidProto() *mpb.Masjid {
+	return &mpb.Masjid{
+		Id:          DefaultId,
+		Name:        MasjidName,
+		IsVerified:  false,
+		Address:     &mpb.Masjid_Address{},
+		PhoneNumber: &mpb.Masjid_PhoneNumber{},
 	}
 }
