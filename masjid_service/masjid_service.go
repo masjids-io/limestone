8cd32b77886ce77edd55f2c85512bbe4c0e4ce96
mnadev <mohammednadeem902@gmail.com>
2024-05-25 20:52:13 -0400

    small readability changes

--- masjid_service/masjid_service.go
+++ masjid_service/masjid_service.go
@@ -11,36 +11,36 @@ import (
 )
 
 type MasjidServiceServer struct {
-	SM *storage.StorageManager
+	smgr *storage.StorageManager
 	pb.UnimplementedMasjidServiceServer
 }
 
-func (srvr *MasjidServiceServer) CreateMasjid(ctx context.Context, in *pb.CreateMasjidRequest) (*pb.Masjid, error) {
-	masjid, err := srvr.SM.CreateMasjid(in.GetMasjid())
+func (s *MasjidServiceServer) CreateMasjid(ctx context.Context, in *pb.CreateMasjidRequest) (*pb.Masjid, error) {
+	masjid, err := s.smgr.CreateMasjid(in.GetMasjid())
 	if err != nil {
 		return nil, err
 	}
 	return masjid.ToProto(), status.Error(codes.OK, codes.OK.String())
 }
 
-func (srvr *MasjidServiceServer) GetMasjid(ctx context.Context, in *pb.GetMasjidRequest) (*pb.Masjid, error) {
-	masjid, err := srvr.SM.GetMasjid(in.GetMasjidId())
+func (s *MasjidServiceServer) GetMasjid(ctx context.Context, in *pb.GetMasjidRequest) (*pb.Masjid, error) {
+	masjid, err := s.smgr.GetMasjid(in.GetMasjidId())
 	if err != nil {
 		return nil, err
 	}
 	return masjid.ToProto(), status.Error(codes.OK, codes.OK.String())
 }
 
-func (srvr *MasjidServiceServer) UpdateMasjid(ctx context.Context, in *pb.UpdateMasjidRequest) (*pb.Masjid, error) {
-	masjid, err := srvr.SM.UpdateMasjid(in.GetMasjid())
+func (s *MasjidServiceServer) UpdateMasjid(ctx context.Context, in *pb.UpdateMasjidRequest) (*pb.Masjid, error) {
+	masjid, err := s.smgr.UpdateMasjid(in.GetMasjid())
 	if err != nil {
 		return nil, err
 	}
 	return masjid.ToProto(), status.Error(codes.OK, codes.OK.String())
 }
 
-func (srvr *MasjidServiceServer) DeleteMasjid(ctx context.Context, in *pb.DeleteMasjidRequest) (*pb.DeleteMasjidResponse, error) {
-	err := srvr.SM.DeleteMasjid(in.GetMasjidId())
+func (s *MasjidServiceServer) DeleteMasjid(ctx context.Context, in *pb.DeleteMasjidRequest) (*pb.DeleteMasjidResponse, error) {
+	err := s.smgr.DeleteMasjid(in.GetMasjidId())
 	if err != nil {
 		return nil, err
 	}
