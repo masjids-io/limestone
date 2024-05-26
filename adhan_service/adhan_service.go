5eaa34370e1bedcf73add67334b04687261b05e6
mnadev <mohammednadeem902@gmail.com>
2024-05-25 20:46:43 -0400

    small readability changes

--- adhan_service/adhan_service.go
+++ adhan_service/adhan_service.go
@@ -11,36 +11,36 @@ import (
 )
 
 type AdhanServiceServer struct {
-	SM *storage.StorageManager
+	smgr *storage.StorageManager
 	pb.UnimplementedAdhanServiceServer
 }
 
-func (srvr *AdhanServiceServer) CreateAdhanFile(ctx context.Context, in *pb.CreateAdhanFileRequest) (*pb.AdhanFile, error) {
-	adhan_file, err := srvr.SM.CreateAdhanFile(in.GetAdhanFile())
+func (s *AdhanServiceServer) CreateAdhanFile(ctx context.Context, in *pb.CreateAdhanFileRequest) (*pb.AdhanFile, error) {
+	adhan_file, err := s.smgr.CreateAdhanFile(in.GetAdhanFile())
 	if err != nil {
 		return nil, err
 	}
 	return adhan_file.ToProto(), status.Error(codes.OK, codes.OK.String())
 }
 
-func (srvr *AdhanServiceServer) GetAdhanFile(ctx context.Context, in *pb.GetAdhanFileRequest) (*pb.AdhanFile, error) {
-	adhan_file, err := srvr.SM.GetAdhanFile(in.GetMasjidId())
+func (s *AdhanServiceServer) GetAdhanFile(ctx context.Context, in *pb.GetAdhanFileRequest) (*pb.AdhanFile, error) {
+	adhan_file, err := s.smgr.GetAdhanFile(in.GetMasjidId())
 	if err != nil {
 		return nil, err
 	}
 	return adhan_file.ToProto(), status.Error(codes.OK, codes.OK.String())
 }
 
-func (srvr *AdhanServiceServer) UpdateAdhanFile(ctx context.Context, in *pb.UpdateAdhanFileRequest) (*pb.AdhanFile, error) {
-	adhan_file, err := srvr.SM.UpdateAdhanFile(in.GetAdhanFile())
+func (s *AdhanServiceServer) UpdateAdhanFile(ctx context.Context, in *pb.UpdateAdhanFileRequest) (*pb.AdhanFile, error) {
+	adhan_file, err := s.smgr.UpdateAdhanFile(in.GetAdhanFile())
 	if err != nil {
 		return nil, err
 	}
 	return adhan_file.ToProto(), status.Error(codes.OK, codes.OK.String())
 }
 
-func (srvr *AdhanServiceServer) DeleteAdhanFile(ctx context.Context, in *pb.DeleteAdhanFileRequest) (*pb.DeleteAdhanFileResponse, error) {
-	err := srvr.SM.DeleteAdhanFile(in.GetId())
+func (s *AdhanServiceServer) DeleteAdhanFile(ctx context.Context, in *pb.DeleteAdhanFileRequest) (*pb.DeleteAdhanFileResponse, error) {
+	err := s.smgr.DeleteAdhanFile(in.GetId())
 	if err != nil {
 		return nil, err
 	}
